2f91721c1192eedd58bc9705bf33fdf388c5a192
Seyit Ahmet Kutuzman <seyit19033247@gmail.com>
2020-10-12 18:17:44 +0300

    al kardeşim
    al kardeşim

---
+++ Assets/scripts/Shootter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootter : MonoBehaviour
+{
+    public GameObject bullet;
+    public float force = 20f;
+    public static bool isIns = true;
+    public static bool evir = true;
+    Vector3 offset;
+    private void Update()
+    {
+        if (isIns)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(bullet, transform.position, Quaternion.Euler(45,0,0));
+                isIns = false;
+                evir = false;
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        offset = new Vector3(0, 0, -2);
+        if (!isIns)
+        {
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, bullet.transform.position + offset, 1);
+        }
+    }
+}
---
+++ Assets/scripts/bulletforce.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class bulletforce : MonoBehaviour
+{
+    Vector3 offset;
+    private float forward_speed = 15;
+    public Rigidbody Ball;
+
+    void FixedUpdate()
+    {
+        Ball.MovePosition(transform.position + transform.forward * forward_speed * Time.deltaTime);
+
+    }
+
+
+    Vector3 myposition;
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+            myposition = touchPosition;
+            transform.position = Vector3.Lerp(transform.position, myposition, Time.deltaTime);
+        }
+    }
+    private void LateUpdate()
+    {
+        offset = new Vector3(0, 0, -4);
+        if (Shootter.isIns == false)
+        {
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, gameObject.transform.position + offset, 1);
+        }
+    }
+}
